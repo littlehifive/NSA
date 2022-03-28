525c28a37dff571dec58b2cd303c716281b5c74a
Michael Wu <wuzezhen33@gmail.com>
2022-03-27 23:52:56 -0400

    Kathmandu cleaning and logs

---
+++ R/cleaning.R
@@ -0,0 +1,74 @@
+# cleaning and checking
+
+library(tidyverse)
+
+# 1. Kathmandu ---------------------------------------------------------------
+
+path <- "/Volumes/GoogleDrive/My Drive/Nepal SA Study/Data"
+
+dat_ktm <- openxlsx::read.xlsx(file.path(path, "Baseline/original/Baseline_Survey_NSA_Kathmandu_27032022_v01.xlsx"))
+
+# unique(dat_ktm$confident_abt_future)
+# table(dat_ktm$confident_abt_future, useNA = "always")
+# sapply(dat_ktm[,18:44], table)
+
+dat_ktm <- dat_ktm %>% 
+  rename(worried_other_think = worried_.other_think, 
+         conclusion_my_perform = conclusion_my_.perform,
+         conclusion_abt_me = `_12_conclusion_me`,
+         teacher_like_me = teacher_likeme,
+         comfortable_who_i_am = comfortable_who_iam,
+         not_understand_class = not_.understand_class) %>% 
+  mutate(sch_id = ifelse(sch_id == 2, sch_id, 2)) %>% # there is one labeled 27 instead of 2
+  mutate(std_name = gsub("(.*)(\\s)$", "\\1", std_name),  # remove white space at the end
+         std_name = gsub("([\\w])([\\w]+)", "\\U\\1\\L\\2", std_name, perl = TRUE)) %>% # title case names
+  mutate(grade = as.factor(grade)) %>% # change grade from numeric to factor
+  rename(st_id = std_id) %>% # should be st_id to match what's in the id
+  mutate(st_id = gsub("-", "_", st_id)) %>% # change - to _ in st_id
+  mutate(gender = ifelse(st_id == "SCH2_GR9_ST4", 2, gender), # this should be a girl that is not labelled
+         gender = ifelse(gender == 2, "Female", "Male")) %>%  # change 1/2 to texts
+  mutate(father_edu = case_when(father_edu == 1 ~ "Do not go to school",
+                                father_edu == 2 ~ "Basic education (Class 1 to 8)",
+                                father_edu == 3 ~ "Secondary education (Class 9 to 12)",
+                                father_edu == 4 ~ "Some college education",
+                                father_edu == 5 ~ "Bachelor and above",
+                                father_edu == 6 ~ "Do not know",
+                                father_edu == 7 ~ "Don't want to answer"
+                                )) %>% # labels set to original text values
+  mutate(mother_edu = case_when(mother_edu == 1 ~ "Do not go to school",
+                                mother_edu == 2 ~ "Basic education (Class 1 to 8)",
+                                mother_edu == 3 ~ "Secondary education (Class 9 to 12)",
+                                mother_edu == 4 ~ "Some college education",
+                                mother_edu == 5 ~ "Bachelor and above",
+                                mother_edu == 6 ~ "Do not know",
+                                mother_edu == 7 ~ "Don't want to answer"
+  )) %>% # labels set to original text values
+  mutate(father_occ = case_when(father_occ == 1 ~ "Self-employment (agriculture)",
+                                father_occ == 2 ~ "Self-employment (non-agriculture/business)",
+                                father_occ == 3 ~ "Agricultural-based labor",
+                                father_occ == 4 ~ "Other labor (Daily wages)",
+                                father_occ == 5 ~ "Regular salary based job (government/job)",
+                                father_occ == 6 ~ "Not involved in any occupation",
+                                father_occ == 7 ~ "Seeking job",
+                                father_occ == 8 ~ "Household work",
+                                father_occ == 9 ~ "Others (please mention)",
+                                father_occ == 10 ~ "Do not want to mention",
+                                father_occ == 11 ~ "Do not know",
+                                )) %>% # labels set to original text values
+  mutate(father_occ = case_when(mother_occ == 1 ~ "Self-employment (agriculture)",
+                                mother_occ == 2 ~ "Self-employment (non-agriculture/business)",
+                                mother_occ == 3 ~ "Agricultural-based labor",
+                                mother_occ == 4 ~ "Other labor (Daily wages)",
+                                mother_occ == 5 ~ "Regular salary based job (government/job)",
+                                mother_occ == 6 ~ "Not involved in any occupation",
+                                mother_occ == 7 ~ "Seeking job",
+                                mother_occ == 8 ~ "Household work",
+                                mother_occ == 9 ~ "Others (please mention)",
+                                mother_occ == 10 ~ "Do not want to mention",
+                                mother_occ == 11 ~ "Do not know",
+  )) %>% # labels set to original text values
+  mutate_at(vars(capable_person:conclusion_abt_me), function(x){ifelse(x == 6, NA_integer_, x)}) %>% # change don't know to NA
+  mutate_at(vars(bad_grades:pressure_parent_teacher), function(x){ifelse(x == 5, NA_integer_, x)}) # change don't know to NA
+  
+  
+  
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# NSA
-Nepal Self-affirmation Study
+# Nepal Self-affirmation Study
+
+## Cleaning Log
+
+**03.27.2022**:
+
+1.  **Kathmandu check:**
+
+The variable names in tab 1 does not correspond perfectly with tab 2. I am going with whatever is the variable names in the actual dataset for now. Also note that there should not be spaces in variable names. The following names are changed (right hand side changed to left hand side):
+
+worried_other_think = worried\_.other_think,
+
+conclusion_my_perform = conclusion_my\_.perform,
+
+conclusion_abt_me = \`\_12_conclusion_me\`,
+
+teacher_like_me = teacher_likeme,
+
+comfortable_who_i\_am = comfortable_who_iam,
+
+not_understand_class = not\_.understand_class
+
+The following errors have been cleaned by me, **several checks are needed from RIDA**:
+
+| Variable name                                                       | Operations                                                                                                                |
+|---------------------------------------------------------------------|---------------------------------------------------------------------------------------------------------------------------|
+| sch_id                                                              | There is one value labeled as 27 instead of 2.                                                                            |
+| std_name                                                            | removed white spaces at the end of the string, and set the names to be in title case.                                     |
+| grade                                                               | changed from numeric to factor (for R analysis only).                                                                     |
+| std_id                                                              | changed to st_id; "-" changed to "\_"                                                                                     |
+| gender                                                              | no gender available for SCH2_GR9_ST4, set to female based on name; 1/2 labels changed to male/female for clarity purposes |
+| age                                                                 | no change; **RIDA please check if age \> 22 is indeed correct**                                                           |
+| father_edu                                                          | labels set to original text values (for R analysis only)                                                                  |
+| mother_edu                                                          | labels set to original text values (for R analysis only)                                                                  |
+| father_occ                                                          | labels set to original text values (for R analysis only)                                                                  |
+| mother_occ                                                          | labels set to original text values (for R analysis only)                                                                  |
+| adult_members                                                       | **RIDA please check if 14 is possible.**                                                                                  |
+| siblings                                                            | **RIDA please check if 27 is possible.**                                                                                  |
+| 5-point survey questions (from capable_person to conclusion_abt_me) | 6 changed to missing (for R analysis only)                                                                                |
+| 4-point survey (from bad_grades to pressure_parent_teacher)         | 5 changed to missing (for R analysis only)                                                                                |
