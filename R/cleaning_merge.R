acd625ac4fbacc3fec1660dd2ba6685c27e28f46
Zezhen Wu <wuzezhen33@gmail.com>
2022-09-05 21:15:12 -0400

    fix ids and names across waves

--- R/cleaning_merge.R
+++ R/cleaning_merge.R
@@ -83,22 +83,49 @@ merge_dat <- function(dat_b_cleaned, dat_e_cleaned, dat_int_cleaned){
   # full join
   dat_all_cleaned <- dat_b_cleaned |>
     full_join(select(dat_e_cleaned, 
-                     -c(student_type_e, sch_name_e, sch_id_e, st_name_e, 
+                     -c(student_type_e, sch_name_e, sch_id_e, 
                         grade_e, gender_e)),
               by = c("st_id_b" = "st_id_e")) |> 
     full_join(select(dat_int_cleaned, 
-                     -c(sch_id_int, grade_int, st_name_int)),
+                     -c(sch_id_int, grade_int)),
               by = c("st_id_b" = "st_id_int"))
+  
+  
+  # retain st_name in baseline or intervention when baseline is missing
+  dat_all_cleaned <- dat_all_cleaned |> 
+    mutate(st_name = ifelse(is.na(st_name_b) == T,
+                            ifelse(is.na(st_name_e) == T, st_name_int, st_name_e),
+                            st_name_b))
  
   # rename variables that don't change across waves
   dat_all_cleaned <- dat_all_cleaned |> 
+    select(-c(st_name_b, st_name_e, st_name_int)) |> 
     rename_with(.cols = c(student_type_b, sch_name_b:gender_b, 
                           father_edu_b:siblings_b),
                 .fn = ~ gsub("_b", "", .x))
   
+  
+  # add sch_name, sch_id, grade for those only with data in intervention
+  dat_all_cleaned <- dat_all_cleaned |> 
+    mutate(sch_id = gsub("^SCH(\\d+)_GR(\\d+)_ST(\\d+)$", "\\1", st_id),
+           sch_id = as.character(sch_id)) |>
+    mutate(grade = gsub("^SCH(\\d+)_GR(\\d+)_ST(\\d+)$", "\\2", st_id),
+           grade = as.factor(as.numeric(grade))) |> 
+    mutate(sch_name = case_when(
+      sch_id == "1" ~ "Dhawalagiri Higher School for Deaf, Baglung",
+      sch_id == "2" ~ "Central School for Deaf, Kathmandu",
+      sch_id == "3" ~ "Shree Sirjana Deaf School, Pokhara"
+    )) 
+  
+  # recreate ids
+  dat_all_cleaned <- dat_all_cleaned |> 
+    mutate(temp_id = as.numeric(gsub("^SCH(\\d+)_GR(\\d+)_ST(\\d+)$", "\\3", st_id))) |> 
+    arrange(sch_id, grade, temp_id) |>  # sort by grade and student ID
+    select(-temp_id)
+  
   # reorder variables
   dat_all_cleaned <- dat_all_cleaned |> 
-    select(sch_name:gender, age_b, age_e, student_type, 
+    select(sch_name:st_id, st_name, gender, age_b, age_e, student_type, 
            father_edu:siblings,
            form_int:notes_int,
            capable_person_b:notes_b,
